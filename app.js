973f092ba5f6755f5210d524caf292e4536d100a
Zentor88 <43510535+Zentor88@users.noreply.github.com>
2022-09-12 17:25:56 -0500

    basic db setup and seeding

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
 const express = require('express');
-const app = express();
 const PORT = 3000;
+const path = require('path');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground')
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection Error:"));
+db.once("open", () => {
+    console.log("Connected to Database");
+});
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
+app.get('/', (req, res) => {
+    res.render('home');
+})
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({})
+    res.render('campgrounds/index', { campgrounds })
+})
 
 app.listen(PORT, ()=> {
     console.log(`Server up on Port ${PORT}`);
